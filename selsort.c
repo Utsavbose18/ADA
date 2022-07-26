3aee21f397206dbcab15ca8eb4389be8387d5c1d
Utsavbose18 <84223457+Utsavbose18@users.noreply.github.com>
2022-07-26 19:46:51 +0530

    Add files via upload

---
+++ selsort.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+void selsort(int n, int a[])
+void main()
+{
+    int a[15000]i,j,n,ch,temp;
+    clock_t start,end;
+    while(1)
+    {
+
+        printf("\n1: enter the manual entry of N value and the array elememts");
+        printf("\n2: display the time taken for sorting the element in N range  from 500 to 14500");
+        printf("\n3: to exit");
+        printf( )
+    }
+}
